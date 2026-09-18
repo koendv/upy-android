4bbfb237e4bcb52f8a5e94c8f406a7c4c5a5b8ac
Koen De Vleeschauwer <kdv@kdvelectronics.eu>
2026-09-18 08:16:37 +0200

    gradle dependency locking

--- upy-android/app/build.gradle.kts
+++ upy-android/app/build.gradle.kts
@@ -58,6 +58,10 @@ android {
     }
 }
 
+dependencyLocking {
+    lockAllConfigurations()
+}
+
 dependencies {
     implementation(platform("androidx.compose:compose-bom:2026.09.00"))
     implementation("androidx.compose.ui:ui")
